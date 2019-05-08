71c25f861e6b820e59e2286c4492f9dcc8f747d7
Artyom <taranenko@grechka.digital>
2019-05-08 19:07:47 +0300

    adaptive slider start

--- js/common.js
+++ js/common.js
@@ -51,14 +51,31 @@ $(function() {
 		imgSvg();
 	});
 });
-$(document).ready(function() {
+$(window).on('wheel', function (e) {
 	//Scroll
-	// var height = $(window).height() + 10;
+	var headerGroup = $('.header__contacts, .header__lang, .header__logo');
+	var translateGroup = $('body, .header, .main__line');
+	// var delta = e.originalEvent.wheelDelta || e.originalEvent.deltaY || e.originalEvent.detail;
+	if( e.originalEvent.wheelDelta < 0 || e.originalEvent.deltaY > 0 ) {
+		if( !headerGroup.hasClass('translate') ) {
+			translateGroup.addClass('translate');
+			setTimeout(function(){
+				headerGroup.addClass('gray');
+			}, 200);
+		}
+	} else {
+		translateGroup.removeClass('translate');
+		setTimeout(function(){
+			headerGroup.removeClass('gray');
+		}, 200);
+	}
+});
+$(document).ready(function() {
+	//Click
 	var headerGroup = $('.header__contacts, .header__lang, .header__logo');
 	var translateGroup = $('body, .header, .main__line');
 	$('.main-bg-footer__btn-go').on('click', function() {
 		translateGroup.addClass('translate');
-		// $('').addClass('translate');
 		setTimeout(function(){
 			headerGroup.addClass('gray');
 		}, 200);
@@ -120,17 +137,15 @@ $(document).ready(function() {
 		if(sliderlength && sliderNext.length) sliderNext.css('display', sliderItem.last().hasClass('slick-active') ? 'none' : 'block');
 	}
 });
-var height = $(window).height() - 64;
-function updateScroll(scroll) {
-	if( scroll > height ) {
-		if( !$('.header').hasClass('header_dark') ) {
-			$('.header').addClass('header_dark');
-		}
-	} else {
-		$('.header').removeClass('header_dark');
-	}
-}
-$(window).on('scroll', function () {
-	updateScroll($(this).scrollTop());
-});
+// var height = $(window).height() - 64;
+// function updateScroll(scroll) {
+// 	if( scroll > height ) {
+// 		if( !$('.header').hasClass('header_dark') ) {
+// 			$('.header').addClass('header_dark');
+// 		}
+// 	} else {
+// 		$('.header').removeClass('header_dark');
+// 	}
+// }
+
 
